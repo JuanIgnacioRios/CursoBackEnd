68f65bd9c89e5981f1c5e3d9cf08be6e9a59ecce
Juan Ignacio Rios <juanignaciorios2003@gmail.com>
2024-04-09 23:30:24 -0300

    avance desafio7

--- Desafios/Desafio7/src/routes/views.router.js
+++ Desafios/Desafio7/src/routes/views.router.js
@@ -1,50 +1,12 @@
 import express from 'express'
-import ProductManager from '../services/ProductManager.js';
-import CartManager from '../services/CartManager.js';
+import viewsController from '../controllers/views.controller';
 
 const router = express.Router();
 
-const ProductsManager = new ProductManager();
-const CartsManager = new CartManager();
-
-router.get('/products', async (req, res) => {
-    if(!req.session.user){
-        return res.redirect('/login')
-    }
-
-    let { limit, page, sort, query } = req.query;
-    if (!limit) limit = 10;
-    if (!page) page = 1;
-    let result = await ProductsManager.getProducts(limit, parseInt(page), sort, query)
-    let Productos = result.payload.docs
-
-    const {first_name, last_name, email, age, isAdmin} = req.session.user
-    res.render("products",{ Products: Productos, first_name, last_name, email, age, isAdmin })
-})
-
-router.get('/login', (req, res) =>{
-    res.render("login")
-})
-
-router.get('/register', (req, res) =>{
-    res.render("register")
-})
-
-router.get('/cart/:id', async (req,res)=>{
-    const cartId = req.params.id
-    let result = await CartsManager.getCartById(cartId)
-    
-    let Productos = [];
-    for (const product of result.payload[0].products) {
-        let productName = await ProductsManager.getProductById(product.productId.toString());
-        Productos.push({
-            productId: productName.payload.title,
-            quantity: product.quantity
-        });
-    }
-    
-    res.render("cart",{ Products: Productos })
-})
+router.get('/products', viewsController.renderProducts)
+router.get('/login', viewsController.renderLogin)
+router.get('/register', viewsController.renderRegister)
+router.get('/cart/:id', viewsController.renderCart)
 
 
 export default router
